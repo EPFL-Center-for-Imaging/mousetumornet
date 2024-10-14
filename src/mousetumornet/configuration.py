ee978bd6dc1beff4f361ca54b171b84a14197989
Mallory Wittwer <mallory.wittwer@epfl.ch>
2024-10-14 10:10:58 +0200

    Add model Oct-24

--- src/mousetumornet/configuration.py
+++ src/mousetumornet/configuration.py
@@ -1,10 +1,8 @@
 MIN_SIZE_PX = 150
 
 MODELS = {
-#     'v40': ("https://zenodo.org/record/7961832/files/nnunetv2_weights_fullres.zip",
-#             "d26e446d7b95934c958f6a4cb23ced5399c636c599463ebe414d76474588d442"),
-#     'v41': ("https://zenodo.org/record/7961832/files/nnunetv2_export_fullres256.zip",
-#            "4ed6edc11b4330d4d14351076c2a5039b29cc4ad61336d0c83666d9b2ce2b661"),
     'v42': ("https://zenodo.org/records/10074441/files/nnunetv2_export_fullres_nov23.zip",
-           "e343b4808a3741b0b5752961b1225e88ab96519c22510bfcc0726c597213ed1e")
+           "e343b4808a3741b0b5752961b1225e88ab96519c22510bfcc0726c597213ed1e"),
+    'oct24': ("https://zenodo.org/records/13927724/files/model-oct-2024.zip",
+              "md5:99a600884b3b3e4dd0302af045d06a62"),
 }
